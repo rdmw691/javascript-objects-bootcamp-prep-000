3ee689541befb788a912e2522d299cb587092b1b
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-14 17:56:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 
 function updatePlaylist(playlist, artist, song) {
-  return Object.assign({}, playlist, {[artist]: song})
+  return Object.assign(playlist, {[artist]: song})
 }
 
 function removeFromPlaylist(playlist, artist) {
